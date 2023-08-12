40af2b96a170b04b1026b6e2c7bbfa0b05916b2a
Abir0248 <abir15-6789@diu.edu.bd>
2023-08-13 05:51:24 +0600

    3 and questions as comment

--- 1 for in loop.js
+++ 1 for in loop.js
@@ -17,3 +17,9 @@ const emptyObject = {};
 
 printObjectProperties(person);
 printObjectProperties(emptyObject);
+
+/*1) javascript For in loop
+
+
+Write a JavaScript function named printObjectProperties that takes an object as an argument and uses the for...in loop to print all the properties and their values in the following format: "property: value". If the object is empty, print "Object is empty".
+*/
--- 2 return max number.js
+++ 2 return max number.js
@@ -21,3 +21,15 @@ console.log(findMaxNumber(numbers2));
 
 const emptyArray = [];
 console.log(findMaxNumber(emptyArray));
+
+/* 2) javascript function returns
+Write a JavaScript function named findMaxNumber that takes an array of numbers as an argument and returns the maximum number in the array. If the array is empty, the function should return null.
+
+Example:
+const numbers1 = [10, 20, 5, 30, 25];
+console.log(findMaxNumber(numbers1)); // Output: 30
+const numbers2 = [5, 15, 7];
+console.log(findMaxNumber(numbers2)); // Output: 15
+const emptyArray = [];
+console.log(findMaxNumber(emptyArray)); // Output: null
+*/
